f189482a20bd92d228d214d26c804ed7e962c71d
Markus Bilz <markus.bilz@student.kit.edu>
2021-06-30 08:55:46 +0100

    Simplified Call Implementation + Impr. docs 🫖
    - Added references to papers
    - Removed redundant code

--- call.py
+++ call.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 from scipy.stats import norm
 
@@ -10,12 +8,14 @@ class Call(object):
         """
         This is the constructor for a European call option.
 
+        Object can be used to calculate prices using Black Scholes and Cox Ross Rubinstein
+
         :param s_0: current stock price e. g. 15,673.64
-        :param K: strike price
-        :param r: risk-free interest rate e. g. EURIBOR rate
-        :param sigma: volatility
-        :param T: time to maturity
-        :param subscription_ratio: subscription ratio
+        :param K: strike price e. g. 15,100
+        :param r: risk-free interest rate e. g. EURIBOR rate e. g. -0.00513
+        :param sigma: volatility e. g. 0.1862
+        :param T: time to maturity e. g. 0.05
+        :param subscription_ratio: subscription ratio e. g. 0.01
         """
 
         self._s_0 = s_0
@@ -26,6 +26,7 @@ class Call(object):
         self._C = 0
         self._u = 0
         self._d = 0
+        self._p = 0
         self._n = 0
         self._subscription_ratio = subscription_ratio
 
@@ -41,11 +42,15 @@ class Call(object):
             year = {1973},
             pages = {637--654},
 	    }
+
+        :rtype: float
+        :param t: starting point in time, default is 0
         :return: call_price
         """
 
+        # See p. 640 following
         delta_t = self._T - t
-        sigma_sqrt_t = self._sigma * math.sqrt(delta_t)
+        sigma_sqrt_t = self._sigma * np.sqrt(delta_t)
 
         d_1 = (np.log(self._s_0 / self._K) + (self._r + 0.5 * self._sigma ** 2) * delta_t) / (sigma_sqrt_t)
         d_2 = d_1 - sigma_sqrt_t
@@ -56,7 +61,17 @@ class Call(object):
 
     def get_crr(self, n: int) -> float:
         """
-        Matrix-based implementation of a binomial tree as described in:
+        Function calculate the option price of a call option using a binomial tree as described in:
+
+        and
+        @article{
+            title = {{OPTION} {PRICING}: {A} {SIMPLIFIED} {APPROACH}},
+            author = {Cox, John C and Ross, A},
+            pages = {35},
+        }
+
+        Idea for matrix-based implementation is inspired from:
+
         @article{gilli_implementing_2009,
             title = {Implementing {Binomial} {Trees}},
             issn = {1556-5068},
@@ -66,57 +81,50 @@ class Call(object):
             year = {2009},
         }
 
-        and
-        @article{
-            title = {{OPTION} {PRICING}: {A} {SIMPLIFIED} {APPROACH}},
-            author = {Cox, John C and Ross, A},
-            pages = {35},
-        }
 
-        :return:
+        :param n: number of time steps
+        :return: call_price
         """
 
-        # FIXME: Replace with more efficient implementation?
-
         self._n = int(n)
-        self._u = math.exp(self._sigma * self._T / self._n)
-        self._d = 1 / self._u
 
-        # see script chap. formerly 3.2 Binomialmodell (p. 125)
-        # see cox paper p. 234
+        # see p. 232 following
         delta_t = self._T / self._n
+        sigma_sqrt_t = self._sigma * np.sqrt(delta_t)
 
-        u = np.e ** (self._sigma * np.sqrt(delta_t))
-        d = 1.0 / u
-        p = (np.e ** (self._r * delta_t) - d) / (u - d)
+        self._u = np.exp(sigma_sqrt_t)
+        self._d = 1.0 / self._u
+        self._p = (np.exp(self._r * delta_t) - self._d) / (self._u - self._d)
 
-        # build rectangular matrix
+        # build rectangular matrix for asset prices. Use NaNs as default values for better visualization.
         stock_matrix = np.zeros(shape=(self._n + 1, self._n + 1))
         stock_matrix.fill(np.nan)
 
+        # create second matrix for option prices
         option_matrix = stock_matrix.copy()
 
         # build asset tree, populate only upper triangle matrix
         for i in range(self._n + 1):
             for j in range(i + 1):
-                # S_{i,j} = S u^j d^{i-j}
-                stock_matrix[j, i] = self._s_0 * (u ** (i - j)) * (d ** j)
-
-        # print("Asset Tree")
-        # print(stock_matrix)
+                stock_matrix[j, i] = self._s_0 * (self._u ** (i - j)) * (self._d ** j)
 
         # calculate pay-off for entire, very last column of matrix
         option_matrix[:, self._n] = np.maximum(np.zeros(self._n + 1), (stock_matrix[:, self._n] - self._K))
 
-        # calc prices in matrix matrix recursively. Start at very end.
+        # calculate prices in matrix recursively. Start at very end and iterate to the very left.
         for i in range(self._n - 1, -1, -1):
             for j in range(i + 1):
-                # see page 234 of cox paper
-                option_matrix[j, i] = (p * option_matrix[j, i + 1] + (1 - p) * option_matrix[j + 1, i + 1]) * np.exp(
+                # see p. 234
+                option_matrix[j, i] = (self._p * option_matrix[j, i + 1] + (1 - self._p) * option_matrix[
+                    j + 1, i + 1]) * np.exp(
                     -self._r * (self._T / self._n))
 
         # return very first price as current option value
         call = option_matrix[0, 0]
+
+        # print("Asset Tree")
+        # print(stock_matrix)
+
         # print("Option Tree")
         # print(option_matrix)
 
@@ -125,6 +133,10 @@ class Call(object):
         return call * self._subscription_ratio
 
     def __str__(self) -> str:
+        """
+        Function to return string representation of object.
+        :return: string with object info
+        """
         return f"Call with s_0:{self._s_0}, K:{self._K}, sigma:{self._sigma}, " \
                f"r:{self._r}, n:{self._n}, T:{self._T} and " \
                f"subscription ratio:{self._subscription_ratio}."
