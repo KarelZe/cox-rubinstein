400a19a7d72b51a259640d0058beec438fb1a270
Markus Bilz <markus.bilz@student.kit.edu>
2021-06-30 08:09:30 +0100

    Update params + docs + new plots
    - Update params to be consistent to bonus exercise
    - added proper documentation to test.py
    - created new plots for solution file
    - compared results to DerivaGem (Hull; 2019)

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def main():
     Function to test correctness of program and create required plots
     :return: None
     """
-    european_call = Call(s_0=15673.64, K=15100, r=-0.0513, sigma=0.1862, T=0.5, subscription_ratio=0.01)
+    european_call = Call(s_0=15673.64, K=15100, r=-0.00513, sigma=0.1862, T=0.5, subscription_ratio=0.01)
     print(european_call)
 
     time_steps = np.linspace(1, 1000, 1000)
@@ -21,11 +21,17 @@ def main():
     df_option_prices['bs'] = european_call.get_bs()
     df_option_prices['crr'] = df_option_prices.apply(lambda x: european_call.get_crr(x.name), axis=1)
 
+    # c) part
+    print("solutions to c):")
+    print(f"BS: {european_call.get_bs()}")
+    print(f"CRR: {european_call.get_crr(100)}")
+
+    # d) part
     df_option_prices.plot(kind='line', logx=True, figsize=(16, 9), title="comparison CRR, BS and Market Price",
                           xlabel="periods", ylabel="price")
     plt.show()
 
-    print(df_option_prices.head())
+
 
 if __name__ == "__main__":
     main()
